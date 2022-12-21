178b43207188fea2f632586d8f9c997c6082ebe7
Jessé Silva <jessepescador22@gmail.com>
2022-12-20 21:53:55 -0300

    Foi realizado a solução do exercicio

--- controllers/formulario-controller.js
+++ controllers/formulario-controller.js
@@ -1,8 +1,18 @@
-function OnSubmit(e) {
-    console.log({
-        name: 'Tayrone',
-        password: '123456',
-        email: 'tayrone@thrower.com.br',
-        date: '25/07/1995'
-    })
+
+function OnSubmit(event) {
+  event.preventDefault() 
+
+  let values = {} 
+
+  for (input of event.target) {  
+    values[input.name] = input.value 
+  }
+
+  let formatacao = values.date.split("-") 
+  values.date = formatacao[2]+
+              "/"+formatacao[1]+
+              "/"+formatacao[0]
+
+  console.log(values)
+  
 }
