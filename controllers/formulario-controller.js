94c25ea2f4227ee31874ffb9b98808198195f760
Jessé Silva <jessepescador22@gmail.com>
2022-12-30 20:08:43 -0300

    atualização

--- controllers/formulario-controller.js
+++ controllers/formulario-controller.js
@@ -1,14 +1,14 @@
 
 function OnSubmit(event) {
-  event.preventDefault() 
+  event.preventDefault() // vai realizar o bloqueio da pagina
 
-  let values = {} 
+  let values = {}//criei uma variavel let , vazio pra receber os valores.....do objeto
 
-  for (input of event.target) {  
-    values[input.name] = input.value 
+  for (input of event.target) { // aqui ele está pegando todos os inputs do formulário.
+    values[input.name] = input.value //pegando o nome como a chave e o value como valor
   }
 
-  let formatacao = values.date.split("-") 
+  let formatacao = values.date.split("") 
   values.date = formatacao[2]+
               "/"+formatacao[1]+
               "/"+formatacao[0]
