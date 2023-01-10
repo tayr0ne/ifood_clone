6710cb9d9a08ff2f374b20eded69b08d325f1bf5
Jessé Silva <jessepescador22@gmail.com>
2023-01-09 21:27:43 -0300

    Exercicio Feito

--- controllers/loja-controller.js
+++ controllers/loja-controller.js
@@ -11,7 +11,7 @@ function Desconto(tipo, valor) {
   if (tipo == 'pix') {
     valorFinal = valor - ((valor / 100) * DescPix)
   }
-  
+
   if (tipo == 'credito') {
     valorFinal = valor - ((valor / 100) * DescCred)
   }
@@ -35,6 +35,37 @@ function Desconto(tipo, valor) {
   return valorFinal
 }
 
+function faturamento(tipo, valor) {
+
+  const DescPix = 0;
+  const DescDin = 0;
+  const DescCred = 1.5;
+  const DescDeb = 1;
+  const imposto = 6;
+  let faturamentoLoja = 0;
+
+  if (tipo == 'pix'){
+    faturamentoLoja = valor - ((valor / 100) * imposto) - ((valor / 100) *  DescPix);
+   }
+
+
+  if (tipo == 'credito'){
+   faturamentoLoja = valor- ((valor / 100) * DescCred) - ((valor / 100) * imposto) - 4.99;
+  }
+
+  if (tipo == 'debito'){
+    faturamentoLoja = valor - ((valor / 100) * DescDeb) - ((valor / 100) * imposto) - 2.99;
+   }
+
+   if (tipo == 'dinheiro'){
+    faturamentoLoja = valor - ((valor / 100) * imposto) - ((valor / 100) * DescDin);
+   }
+
+  return faturamentoLoja;
+
+  }
+
+
 function OnSubmit(event) {
   event.preventDefault()
 
@@ -44,7 +75,101 @@ function OnSubmit(event) {
     values[input.name] = input.value
   }
 
-  let valorFinal = Desconto(values.tipoPagamento, values.valor)
+  let valorFinal = Desconto(values.tipoPagamento, values.valor);
+  let faturamentoLoja = faturamento(values.tipoPagamento, values.valor);
 
-  console.log(valorFinal)
+
+  console.log('Valor da Compra Foi de: '+ valorFinal)
+  console.log('Faturamento da loja é: ' + faturamentoLoja);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Desconto(tipo, valor) {
+
+//   const DescMaior500 = 1.5
+//   const DescPix = 2
+//   const DescDin = 5
+//   const DescCred = 0
+//   const DescDeb = 0
+
+//   if (tipo == 'pix') {
+//     valorFinal = valor - ((valor / 100) * DescPix)
+//   }
+  
+//   if (tipo == 'credito') {
+//     valorFinal = valor - ((valor / 100) * DescCred)
+//   }
+
+//   if (tipo == 'debito') {
+//     valorFinal = valor - ((valor / 100) * DescDeb)
+//   }
+
+//   if (tipo == 'dinheiro') {
+//     valorFinal = valor - ((valor / 100) * DescDin)
+//   }
+
+//   if (tipo != 'pix' && tipo != 'credito' && tipo != 'debito' && tipo != 'dinheiro') {
+//     valorFinal = valor
+//   }
+
+//   if (valor >= 500) {
+//     valorFinal = valorFinal - ((valor / 100) * DescMaior500)
+//   }
+
+//   return valorFinal
+// }
+
+
+
+  
+// function OnSubmit(event) {
+//   event.preventDefault()
+
+//   let values = {}
+
+//   for (input of event.target) {
+//     values[input.name] = input.value
+//   }
+
+//   const imposto = 0.06;
+
+//   let valorFinal = Desconto(values.tipoPagamento, values.valor) ;
+//   let valorFaturamento = faturamento(values.tipoPagamento, values.valor);
+
+//   console.log('Valor total da compra é:  ', valorFinal);
+//   console.log('Faturamento da Loja é: ' ,valorFaturamento);
+// }
